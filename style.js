846a9c53eee90d045a2f046ff2dd6c2d2d2d70ab
anilbolukbas <anilbolukbas@gmail.com>
2020-03-03 12:27:50 +0800

    Add files via upload

---
+++ style.js
@@ -0,0 +1,6 @@
+function generatePie() {
+  var image = document.createElement("img");
+  var div = document.getElementById("flex-box-gen");
+  image.src = "https://media.giphy.com/media/pdfnRGpNQzePC/giphy.gif";
+  div.appendChild(image);
+}
